c211ab4f249b37fc5bce1a7fd22387cdb4cbf42f
Erich Hoss <ehoss193@mtroyal.ca>
2020-02-13 15:50:28 -0700

    Removed problem part of test code.

--- test/RangeTest.java
+++ test/RangeTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
 
 class RangeTest {
 	private Range exampleRange;
